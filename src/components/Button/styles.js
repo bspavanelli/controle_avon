d2520ea3a0936d27613885f24514222e16d63548
Bruno dos Santos Pavanelli <bruno.pavanelli@C02S396JFVH3.local>
2020-08-11 21:23:21 -0300

    change button color when disabled

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const ButtonField = styled.button`
   box-shadow: inset 0px 1px 0px 0px #89bff5;
   background: linear-gradient(to bottom, #0b96e0 5%, #0061a7 100%);
-  background-color: #0b96e0;
   border-radius: 5px;
   border: 1px solid #124d77;
   display: inline-block;
@@ -22,5 +21,11 @@ export const ButtonField = styled.button`
   &:active {
     position: relative;
     top: 1px;
+  }
+
+  &:disabled {
+    background: linear-gradient(to bottom, #999999 5%, #555555 100%);
+    box-shadow: inset 0px 1px 0px 0px #aaaaaa;
+    border: 1px solid #888888;
   }
 `;
